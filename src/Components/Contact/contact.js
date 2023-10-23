7f6c2b23d17f4b9b374039400c27b97e8e2acc9d
Adithyan <sadithyan2@gmail.com>
2023-10-23 19:18:46 +0530

    second commit

--- src/Components/Contact/contact.js
+++ src/Components/Contact/contact.js
@@ -2,6 +2,10 @@ import React,{useRef} from 'react'
 // import facebookIcon from '../../assets/'
 import './contact.css'
 import emailjs from '@emailjs/browser';
+import FaceBook from '../../assets/facebook.png';
+import Instagram from '../../assets/instagram.png';
+import Twitter from '../../assets/twitter.png';
+import Youtube from '../../assets/youtube.png'
 // import emailjs from 'emailjs-com';
 
 
@@ -24,7 +28,6 @@ const Contact = () => {
 
   return (
 <section className='contactPage'>
-enecccbklknlbefkdvdeveuhlvgllruuddjbrluddfug
 
 <div id='contact'>
     <h1 className='contactPageTile'>Contact Me</h1>
@@ -39,10 +42,10 @@ enecccbklknlbefkdvdeveuhlvgllruuddjbrluddfug
 <button type='submit' className='submitBtn' value='send'>Submit</button>
 <div className='links'>
 
-    <img src='' alt='' className=''/>
-    <img src='' alt='' className=''/>
-    <img src='' alt='' className=''/>
-    <img src='' alt='' className=''/>
+    <img src={FaceBook} alt='facebook' className='link'/>
+    <img src={Instagram} alt='instagram' className='link'/>
+    <img src={Youtube} alt='youtube' className='link'/>
+    <img src={Twitter} alt='twitter' className='link'/>
 
 </div>
     </form>
